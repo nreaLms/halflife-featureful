9a1813ab6c6dc7cc7b7fabd69e89210ce1696238
Roman Chistokhodov <freeslave93@gmail.com>
2018-09-04 00:37:00 +0300

    Add start off flag for wall chargers

--- dlls/healthkit.cpp
+++ dlls/healthkit.cpp
@@ -97,6 +97,8 @@ BOOL CHealthKit::MyTouch( CBasePlayer *pPlayer )
 // Base class for wall chargers
 //-------------------------------------------------------------
 
+#define SF_WALLCHARGER_STARTOFF 1
+
 void CWallCharger::Spawn()
 {
 	Precache();
@@ -107,8 +109,16 @@ void CWallCharger::Spawn()
 	UTIL_SetOrigin( pev, pev->origin );		// set size and link into world
 	UTIL_SetSize( pev, pev->mins, pev->maxs );
 	SET_MODEL( ENT( pev ), STRING( pev->model ) );
-	m_iJuice = ChargerCapacity();
-	pev->frame = 0;
+	if (FBitSet(pev->spawnflags, SF_WALLCHARGER_STARTOFF))
+	{
+		m_iJuice = 0;
+		pev->frame = 1;
+	}
+	else
+	{
+		m_iJuice = ChargerCapacity();
+		pev->frame = 0;
+	}
 }
 
 void CWallCharger::Precache()
