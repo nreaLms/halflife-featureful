a92cddee3e7e6ca71a57c4504f75ec9943c36914
Roman Chistokhodov <freeslave93@gmail.com>
2021-01-30 18:28:17 +0300

    Add 'Force idle looping' for scripted_sequence

--- dlls/schedule.cpp
+++ dlls/schedule.cpp
@@ -562,6 +562,11 @@ void CBaseMonster::RunTask( Task_t *pTask )
 				pev->framerate = 1.0;
 				//ALERT( at_aiconsole, "Script %s has begun for %s\n", STRING( m_pCine->m_iszPlay ), STRING( pev->classname ) );
 			}
+			else if ( FBitSet(m_pCine->pev->spawnflags, SF_SCRIPT_FORCE_IDLE_LOOPING) && !FStringNull( m_pCine->m_iszIdle) )
+			{
+				if ( m_fSequenceFinished )
+					m_pCine->StartSequence( this, m_pCine->m_iszIdle, FALSE );
+			}
 			break;
 		}
 	case TASK_PLAY_SCRIPT:
