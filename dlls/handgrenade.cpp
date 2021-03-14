ef81e90c052e1abbb853a7a53464ef3b83855fdd
Roman Chistokhodov <freeslave93@gmail.com>
2021-03-14 15:42:42 +0300

    Merge https://github.com/Solokiller/halflife-updated/commit/4bb1f3c320a4b3439db45301510445ea30b43c5e

--- dlls/handgrenade.cpp
+++ dlls/handgrenade.cpp
@@ -175,7 +175,7 @@ void CHandGrenade::WeaponIdle( void )
 		// player "shoot" animation
 		m_pPlayer->SetAnimation( PLAYER_ATTACK1 );
 
-		m_flReleaseThrow = 0.0f;
+		//m_flReleaseThrow = 0.0f;
 		m_flStartThrow = 0.0f;
 		m_flNextPrimaryAttack = GetNextAttackDelay( 0.5f );
 		m_flTimeWeaponIdle = UTIL_WeaponTimeBase() + 0.5f;
