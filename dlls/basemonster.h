22ca8cd9ca5a2cf63c0e6dc8e7b556e092a45386
Roman Chistokhodov <freeslave93@gmail.com>
2022-12-16 06:14:32 +0300

    Disallow NPCs playing scripted sentences when they're dying but not dead yet, unless they're in scripted sequence

--- dlls/basemonster.h
+++ dlls/basemonster.h
@@ -214,7 +214,7 @@ public:
 	virtual void ScheduleChange( void ) {}
 	// virtual int CanPlaySequence( void ) { return ((m_pCine == NULL) && (m_MonsterState == MONSTERSTATE_NONE || m_MonsterState == MONSTERSTATE_IDLE || m_IdealMonsterState == MONSTERSTATE_IDLE)); }
 	virtual int CanPlaySequence( int interruptFlags );
-	virtual int CanPlaySentence( BOOL fDisregardState ) { return IsAlive(); }
+	virtual int CanPlaySentence( BOOL fDisregardState ) { return m_MonsterState == MONSTERSTATE_SCRIPT ? IsAlive() : IsFullyAlive(); }
 	virtual bool PlaySentence( const char *pszSentence, float duration, float volume, float attenuation );
 	virtual void PlayScriptedSentence( const char *pszSentence, float duration, float volume, float attenuation, BOOL bConcurrent, CBaseEntity *pListener );
 
