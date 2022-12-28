4a1bcbe661da2ebcdee9436b5492b920666c9d80
Roman Chistokhodov <freeslave93@gmail.com>
2022-12-28 11:04:03 +0300

    Add CRenderFxFader

--- dlls/triggers.cpp
+++ dlls/triggers.cpp
@@ -655,12 +655,113 @@ void CMultiSequence::Spawn()
 #define SF_RENDER_MASKAMT	( 1 << 1 )
 #define SF_RENDER_MASKMODE	( 1 << 2 )
 #define SF_RENDER_MASKCOLOR	( 1 << 3 )
+//LRC
+#define SF_RENDER_KILLTARGET	(1<<5)
+#define SF_RENDER_ONLYONCE		(1<<6)
+
+
+//LRC-  RenderFxFader, a subsidiary entity for RenderFxManager
+class CRenderFxFader : public CBaseEntity
+{
+public:
+	void Spawn( void );
+	void EXPORT FadeThink ( void );
+	virtual int		Save( CSave &save );
+	virtual int		Restore( CRestore &restore );
+	virtual int	ObjectCaps( void ) { return CBaseEntity :: ObjectCaps() & ~FCAP_ACROSS_TRANSITION; }
+
+	static	TYPEDESCRIPTION m_SaveData[];
+
+	float m_flStartTime;
+	float m_flDuration;
+	float m_flCoarseness;
+	int m_iStartAmt;
+	int m_iOffsetAmt;
+	Vector m_vecStartColor;
+	Vector m_vecOffsetColor;
+	float m_fStartScale;
+	float m_fOffsetScale;
+	EHANDLE m_hTarget;
+
+	int m_iszAmtFactor;
+};
+LINK_ENTITY_TO_CLASS( render_fader, CRenderFxFader )
+
+TYPEDESCRIPTION	CRenderFxFader::m_SaveData[] =
+{
+	DEFINE_FIELD( CRenderFxFader, m_flStartTime, FIELD_FLOAT),
+	DEFINE_FIELD( CRenderFxFader, m_flDuration, FIELD_FLOAT),
+	DEFINE_FIELD( CRenderFxFader, m_flCoarseness, FIELD_FLOAT),
+	DEFINE_FIELD( CRenderFxFader, m_iStartAmt, FIELD_INTEGER),
+	DEFINE_FIELD( CRenderFxFader, m_iOffsetAmt, FIELD_INTEGER ),
+	DEFINE_FIELD( CRenderFxFader, m_vecStartColor, FIELD_VECTOR ),
+	DEFINE_FIELD( CRenderFxFader, m_vecOffsetColor, FIELD_VECTOR ),
+	DEFINE_FIELD( CRenderFxFader, m_fStartScale, FIELD_FLOAT),
+	DEFINE_FIELD( CRenderFxFader, m_fOffsetScale, FIELD_FLOAT ),
+	DEFINE_FIELD( CRenderFxFader, m_hTarget, FIELD_EHANDLE ),
+};
+
+IMPLEMENT_SAVERESTORE(CRenderFxFader, CBaseEntity)
+
+void CRenderFxFader::Spawn( void )
+{
+	SetThink(&CRenderFxFader::FadeThink );
+	pev->classname = MAKE_STRING("render_fader");
+}
+
+void CRenderFxFader::FadeThink( void )
+{
+	if (m_hTarget == 0)
+	{
+//		ALERT(at_console, "render_fader removed\n");
+		SUB_Remove();
+		return;
+	}
+
+	float flDegree = (gpGlobals->time - m_flStartTime)/m_flDuration;
+
+	if (flDegree >= 1)
+	{
+//		ALERT(at_console, "render_fader removes self\n");
+
+		m_hTarget->pev->renderamt = m_iStartAmt + m_iOffsetAmt;
+		m_hTarget->pev->rendercolor = m_vecStartColor + m_vecOffsetColor;
+		m_hTarget->pev->scale = m_fStartScale + m_fOffsetScale;
+
+		SUB_UseTargets( m_hTarget, USE_TOGGLE, 0 );
+
+		if (pev->spawnflags & SF_RENDER_KILLTARGET)
+		{
+			m_hTarget->SetThink(&CRenderFxFader::SUB_Remove);
+			m_hTarget->pev->nextthink = gpGlobals->time + 0.1f;
+		}
+
+		m_hTarget = 0;
+
+		pev->nextthink = gpGlobals->time + 0.1f;
+		SetThink(&CRenderFxFader::SUB_Remove);
+	}
+	else
+	{
+		m_hTarget->pev->renderamt = m_iStartAmt + m_iOffsetAmt * flDegree;
+
+		m_hTarget->pev->rendercolor.x = m_vecStartColor.x + m_vecOffsetColor.x * flDegree;
+		m_hTarget->pev->rendercolor.y = m_vecStartColor.y + m_vecOffsetColor.y * flDegree;
+		m_hTarget->pev->rendercolor.z = m_vecStartColor.z + m_vecOffsetColor.z * flDegree;
+
+		m_hTarget->pev->scale = m_fStartScale + m_fOffsetScale * flDegree;
+
+		pev->nextthink = gpGlobals->time + m_flCoarseness; //?
+	}
+}
 
 class CRenderFxManager : public CBaseEntity
 {
 public:
 	void Spawn( void );
 	void Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
+	void Affect(CBaseEntity *pEntity, bool bIsLocus, CBaseEntity *pActivator );
+	void KeyValue( KeyValueData *pkvd );
 };
 
 LINK_ENTITY_TO_CLASS( env_render, CRenderFxManager )
@@ -670,27 +771,109 @@ void CRenderFxManager::Spawn( void )
 	pev->solid = SOLID_NOT;
 }
 
+void CRenderFxManager :: KeyValue( KeyValueData *pkvd )
+{
+	if (FStrEq(pkvd->szKeyName, "m_fScale"))
+{
+		pev->scale = atof(pkvd->szValue);
+		pkvd->fHandled = TRUE;
+	}
+	else
+		CBaseEntity::KeyValue( pkvd );
+}
+
 void CRenderFxManager::Use( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value )
 {
 	if( !FStringNull( pev->target ) )
 	{
-		edict_t *pentTarget = NULL;
-		while( 1 )
+		CBaseEntity* pTarget = UTIL_FindEntityByTargetname( NULL, STRING(pev->target), pActivator);
+		bool first = true;
+		while ( pTarget )
 		{
-			pentTarget = FIND_ENTITY_BY_TARGETNAME( pentTarget, STRING( pev->target ) );
-			if( FNullEnt( pentTarget ) )
-				break;
+			Affect( pTarget, first, pActivator );
+			first = false;
+			pTarget = UTIL_FindEntityByTargetname( pTarget, STRING(pev->target), pActivator );
+		}
+	}
 
-			entvars_t *pevTarget = VARS( pentTarget );
-			if( !FBitSet( pev->spawnflags, SF_RENDER_MASKFX ) )
-				pevTarget->renderfx = pev->renderfx;
-			if( !FBitSet( pev->spawnflags, SF_RENDER_MASKAMT ) )
-				pevTarget->renderamt = pev->renderamt;
-			if( !FBitSet( pev->spawnflags, SF_RENDER_MASKMODE ) )
-				pevTarget->rendermode = pev->rendermode;
+	if (pev->spawnflags & SF_RENDER_ONLYONCE)
+	{
+		SetThink(&CRenderFxManager::SUB_Remove);
+		pev->nextthink = gpGlobals->time + 0.1f;
+	}
+}
+
+void CRenderFxManager::Affect( CBaseEntity *pTarget, bool bIsFirst, CBaseEntity *pActivator )
+{
+	entvars_t *pevTarget = pTarget->pev;
+
+	float fAmtFactor = 1;
+	if ( pev->message && !FBitSet( pev->spawnflags, SF_RENDER_MASKAMT ) )
+		TryCalcLocus_Ratio(pActivator, STRING(pev->message), fAmtFactor);
+
+	if( !FBitSet( pev->spawnflags, SF_RENDER_MASKFX ) )
+		pevTarget->renderfx = pev->renderfx;
+	if( !FBitSet( pev->spawnflags, SF_RENDER_MASKMODE ) )
+	{
+		//LRC - amt is often 0 when mode is normal. Set it to be fully visible, for fade purposes.
+		if (pev->frags && pevTarget->renderamt == 0 && pevTarget->rendermode == kRenderNormal)
+			pevTarget->renderamt = 255;
+		pevTarget->rendermode = pev->rendermode;
+	}
+	if (pev->frags == 0) // not fading?
+	{
+		if ( !FBitSet( pev->spawnflags, SF_RENDER_MASKAMT ) )
+			pevTarget->renderamt = pev->renderamt * fAmtFactor;
 			if( !FBitSet( pev->spawnflags, SF_RENDER_MASKCOLOR ) )
 				pevTarget->rendercolor = pev->rendercolor;
+		if ( pev->scale )
+			pevTarget->scale = pev->scale;
+
+		if (bIsFirst)
+			FireTargets( STRING(pev->netname), pTarget, this, USE_TOGGLE, 0 );
+	}
+	else
+	{
+		//LRC - fade the entity in/out!
+		// (We create seperate fader entities to do this, one for each entity that needs fading.)
+		CRenderFxFader *pFader = GetClassPtr( (CRenderFxFader *)NULL );
+		pFader->m_hTarget = pTarget;
+		pFader->m_iStartAmt = pevTarget->renderamt;
+		pFader->m_vecStartColor = pevTarget->rendercolor;
+		pFader->m_fStartScale = pevTarget->scale;
+		if (pFader->m_fStartScale == 0)
+			pFader->m_fStartScale = 1; // When we're scaling, 0 is treated as 1. Use 1 as the number to fade from.
+		pFader->pev->spawnflags = pev->spawnflags;
+
+		if (bIsFirst)
+			pFader->pev->target = pev->netname;
+
+		if ( !FBitSet( pev->spawnflags, SF_RENDER_MASKAMT ) )
+			pFader->m_iOffsetAmt = (pev->renderamt * fAmtFactor) - pevTarget->renderamt;
+		else
+			pFader->m_iOffsetAmt = 0;
+
+		if ( !FBitSet( pev->spawnflags, SF_RENDER_MASKCOLOR ) )
+		{
+			pFader->m_vecOffsetColor.x = pev->rendercolor.x - pevTarget->rendercolor.x;
+			pFader->m_vecOffsetColor.y = pev->rendercolor.y - pevTarget->rendercolor.y;
+			pFader->m_vecOffsetColor.z = pev->rendercolor.z - pevTarget->rendercolor.z;
 		}
+		else
+		{
+			pFader->m_vecOffsetColor = g_vecZero;
+		}
+
+		if ( pev->scale )
+			pFader->m_fOffsetScale = pev->scale - pevTarget->scale;
+		else
+			pFader->m_fOffsetScale = 0;
+
+		pFader->m_flStartTime = gpGlobals->time;
+		pFader->m_flDuration = pev->frags;
+		pFader->m_flCoarseness = pev->armorvalue;
+		pFader->pev->nextthink = gpGlobals->time;
+		pFader->Spawn();
 	}
 }
 
