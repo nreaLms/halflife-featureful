2180f04081e8d27add8f037b5db433fe8c0bc8d1
Roman Chistokhodov <freeslave93@gmail.com>
2023-02-21 20:13:03 +0300

    Remove autoaim from portable medkit

--- dlls/medkit.cpp
+++ dlls/medkit.cpp
@@ -116,7 +116,7 @@ int CMedkit::GetItemInfo(ItemInfo *p)
 	p->iPosition = 4;
 	p->iId = m_iId = WEAPON_MEDKIT;
 	p->iWeight = MEDKIT_WEIGHT;
-	
+
 	if (CanRecharge()) {
 		p->iFlags = ITEM_FLAG_NOAUTOSWITCHEMPTY | ITEM_FLAG_NOAUTORELOAD;
 	} else {
@@ -141,12 +141,12 @@ void CMedkit::Holster()
 {
 	m_flSoundDelay = 0;
 	m_pPlayer->m_flNextAttack = UTIL_WeaponTimeBase() + 0.5;
-	
+
 	//HACKHACK - can't select medkit if it's empty! no way to get ammo for it, either
 	if( CanRecharge() && !m_pPlayer->m_rgAmmo[PrimaryAmmoIndex()] ) {
 		m_pPlayer->m_rgAmmo[PrimaryAmmoIndex()] = 1;
 	}
-	
+
 	SendWeaponAnim(MEDKIT_HOLSTER);
 }
 
@@ -210,8 +210,6 @@ void CMedkit::WeaponIdle(void)
 	Reload();
 	ResetEmptySound();
 
-	m_pPlayer->GetAutoaimVector(AUTOAIM_10DEGREES);
-
 	if (m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType] > 0 && m_flSoundDelay != 0 && m_flSoundDelay <= gpGlobals->time)
 	{
 		const int maxHeal = Q_min((int)gSkillData.plrDmgMedkit, m_pPlayer->m_rgAmmo[m_iPrimaryAmmoType]);
@@ -221,9 +219,9 @@ void CMedkit::WeaponIdle(void)
 			EMIT_SOUND_DYN(ENT(pev), CHAN_WEAPON, "items/medshot5.wav", 1.0, ATTN_NORM, 0, 100);
 		} else {
 			m_pPlayer->SetAnimation(PLAYER_ATTACK1);
-			
+
 			CBaseEntity* healTarget = FindHealTarget(true);
-	
+
 			if (healTarget) {
 				const int diff = (int)ceil(healTarget->pev->max_health - healTarget->pev->health);
 				const int healResult = healTarget->TakeHealth(m_pPlayer, Q_min(maxHeal, diff), DMG_GENERIC);
