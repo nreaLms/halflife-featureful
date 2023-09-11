a651d8688ddbfde8db1dc358fc2f868029a6a0f4
Roman Chistokhodov <freeslave93@gmail.com>
2023-09-11 18:45:59 +0300

    Add cl_save_mapname

--- cl_dll/hud.cpp
+++ cl_dll/hud.cpp
@@ -590,6 +590,7 @@ void CHud::Init( void )
 
 	CVAR_CREATE( "zoom_sensitivity_ratio", "1.2", FCVAR_ARCHIVE );
 	CVAR_CREATE( "cl_autowepswitch", "1", FCVAR_ARCHIVE | FCVAR_USERINFO );
+	CVAR_CREATE( "cl_save_mapname", "0", FCVAR_ARCHIVE | FCVAR_USERINFO );
 	default_fov = CVAR_CREATE( "default_fov", "90", FCVAR_ARCHIVE );
 	m_pCvarStealMouse = CVAR_CREATE( "hud_capturemouse", "1", FCVAR_ARCHIVE );
 	m_pCvarDraw = CVAR_CREATE( "hud_draw", "1", FCVAR_ARCHIVE );
