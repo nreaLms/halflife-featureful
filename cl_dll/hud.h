90c87ee093961b9e2ba87ea95636f7d44b814d41
Roman Chistokhodov <freeslave93@gmail.com>
2024-10-21 22:06:56 +0300

    Remove CHud::MsgFunc_Damage (unused function)

--- cl_dll/hud.h
+++ cl_dll/hud.h
@@ -1046,7 +1046,6 @@ public:
 	static HudSpriteRenderer& Renderer();
 
 	// user messages
-	int _cdecl MsgFunc_Damage( const char *pszName, int iSize, void *pbuf );
 	int _cdecl MsgFunc_GameMode( const char *pszName, int iSize, void *pbuf );
 	int _cdecl MsgFunc_Logo( const char *pszName,  int iSize, void *pbuf );
 	int _cdecl MsgFunc_ResetHUD( const char *pszName,  int iSize, void *pbuf );
